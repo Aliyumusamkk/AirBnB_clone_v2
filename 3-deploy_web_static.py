81a0381ada08b4703e5843ce6f9d929b372103b7
aliyumusamkk <aliyumusakalakala@gmail.com>
2023-12-09 13:59:21 -0800

    web server

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['100.26.18.25', '3.85.33.170']
 
 
 @runs_once
